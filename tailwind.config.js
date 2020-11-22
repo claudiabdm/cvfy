9917a59869a95144915da8740207c14e6abde59d
claudiabdm <claudiabdm@hotmail.es>
2020-11-22 21:04:29 +0100

    Add tailwing config

---
+++ tailwind.config.js
@@ -0,0 +1,15 @@
+const defaultTheme = require('tailwindcss/defaultTheme')
+
+module.exports = {
+  theme: {
+    fontFamily: {
+      sans: ['Roboto', 'sans-serif'],
+      serif: ['Roboto Slab', 'serif'],
+    },
+    extend: {
+      colors: {
+        primary: defaultTheme.colors.purple,
+      },
+    },
+  },
+}
