13dd8f2da7eca6a38718d889441411509c942c00
claudiabdm <claudiabdm@hotmail.es>
2020-11-22 21:05:41 +0100

    Add layout

--- pages/index.vue
+++ pages/index.vue
@@ -1,70 +1,55 @@
 <template>
-  <div class="container">
-    <div>
-      <Logo />
-      <h1 class="title">portfolio-generator</h1>
-      <div class="links">
-        <a
-          href="https://nuxtjs.org/"
-          target="_blank"
-          rel="noopener noreferrer"
-          class="button--green"
-        >
-          Documentation
-        </a>
-        <a
-          href="https://github.com/nuxt/nuxt.js"
-          target="_blank"
-          rel="noopener noreferrer"
-          class="button--grey"
-        >
-          GitHub
-        </a>
-      </div>
-    </div>
+  <div class="grid grid-cols-3 h-full">
+    <portfolio-settings
+      id="settings"
+      :form-settings="formSettings"
+      class="col-span-1"
+      @addSkill="onAddSkill($event)"
+      @removeSkill="onRemoveSkill($event)"
+    ></portfolio-settings>
+    <portfolio-preview
+      id="portfolio"
+      :form-settings="formSettings"
+      class="col-span-2"
+    ></portfolio-preview>
   </div>
 </template>
 
 <script lang="ts">
 import Vue from 'vue'
-
-export default Vue.extend({})
+export default Vue.extend({
+  data() {
+    return {
+      formSettings: {
+        jobTitle: 'Front End Developer',
+        name: 'Claudia',
+        lastName: 'Benito',
+        email: 'claudiabdm@hotmail.es',
+        location: 'Madrid, Spain',
+        phoneNumber: '+34 692662091',
+        aboutme: 'Soy clauduuauu',
+        jobSkills: ['Nuxt.js'],
+        softSkills: ['Teamwork'],
+      },
+    }
+  },
+  methods: {
+    onAddSkill(e: {
+      skill: string
+      skillType: 'jobSkills' | 'softSkills'
+    }): void {
+      this.formSettings[e.skillType] = [
+        ...new Set([...this.formSettings[e.skillType], e.skill]),
+      ]
+    },
+    onRemoveSkill(e: {
+      skill: string
+      skillType: 'jobSkills' | 'softSkills'
+    }): void {
+      this.formSettings[e.skillType] = [
+        ...this.formSettings[e.skillType].filter((skill) => skill !== e.skill),
+      ]
+    },
+  },
+})
 </script>
-
-<style>
-/* Sample `apply` at-rules with Tailwind CSS
-.container {
-@apply min-h-screen flex justify-center items-center text-center mx-auto;
-}
-*/
-.container {
-  margin: 0 auto;
-  min-height: 100vh;
-  display: flex;
-  justify-content: center;
-  align-items: center;
-  text-align: center;
-}
-
-.title {
-  font-family: 'Quicksand', 'Source Sans Pro', -apple-system, BlinkMacSystemFont,
-    'Segoe UI', Roboto, 'Helvetica Neue', Arial, sans-serif;
-  display: block;
-  font-weight: 300;
-  font-size: 100px;
-  color: #35495e;
-  letter-spacing: 1px;
-}
-
-.subtitle {
-  font-weight: 300;
-  font-size: 42px;
-  color: #526488;
-  word-spacing: 5px;
-  padding-bottom: 15px;
-}
-
-.links {
-  padding-top: 15px;
-}
-</style>
