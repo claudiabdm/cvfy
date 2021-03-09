11cd64ab9e9ce4098c7a44c04a06f8962288457d
claudiabdm <claudiabdm@hotmail.es>
2021-03-09 20:03:03 +0100

    Add twitter image card

--- .eslintrc.js
+++ .eslintrc.js
@@ -13,5 +13,7 @@ module.exports = {
   ],
   plugins: ['prettier'],
   // add your custom rules here
-  rules: {},
-}
+  rules: {
+    eqeqeq: ['error', 'smart'],
+  },
+};
--- pages/index.vue
+++ pages/index.vue
@@ -18,8 +18,8 @@
 </template>
 
 <script lang="ts">
-import { NuxtOptionsHead } from '@nuxt/types/config/head'
-import Vue from 'vue'
+import { NuxtOptionsHead } from '@nuxt/types/config/head';
+import Vue from 'vue';
 export default Vue.extend({
   data() {
     return {
@@ -77,7 +77,7 @@ export default Vue.extend({
           },
         ],
       },
-    }
+    };
   },
   watch: {
     formSettings: {
@@ -86,89 +86,89 @@ export default Vue.extend({
         localStorage.setItem(
           `cvSettings-${this.$i18n.locale}`,
           JSON.stringify(updated)
-        )
+        );
       },
       deep: true,
     },
   },
   mounted() {
     if (process.browser) {
-      const cvEs = localStorage.getItem('cvSettings-es') || '{}'
-      const cvEn = localStorage.getItem('cvSettings-en') || '{}'
+      const cvEs = localStorage.getItem('cvSettings-es') || '{}';
+      const cvEn = localStorage.getItem('cvSettings-en') || '{}';
       const isCvEsEmpty = this.isCvSettingsFromLocalStorageEmpty(
         JSON.parse(cvEs)
-      )
+      );
       const isCvEnEmpty = this.isCvSettingsFromLocalStorageEmpty(
         JSON.parse(cvEn)
-      )
+      );
       if (this.$i18n.locale.includes('es') && !isCvEsEmpty) {
-        this.formSettings = JSON.parse(cvEs)
+        this.formSettings = JSON.parse(cvEs);
       }
       if (this.$i18n.locale.includes('en') && !isCvEnEmpty) {
-        this.formSettings = JSON.parse(cvEn)
+        this.formSettings = JSON.parse(cvEn);
       }
       if (this.$i18n.locale.includes('es') && isCvEsEmpty && !isCvEnEmpty) {
-        this.formSettings = JSON.parse(cvEn)
-        localStorage.setItem('cvSettings-es', cvEn)
+        this.formSettings = JSON.parse(cvEn);
+        localStorage.setItem('cvSettings-es', cvEn);
       }
       if (this.$i18n.locale.includes('en') && isCvEnEmpty && !isCvEsEmpty) {
-        this.formSettings = JSON.parse(cvEs)
-        localStorage.setItem('cvSettings-en', cvEs)
+        this.formSettings = JSON.parse(cvEs);
+        localStorage.setItem('cvSettings-en', cvEs);
       }
-      this.isLoading = false
+      this.isLoading = false;
     }
   },
   methods: {
     onAddSkill(e: {
-      skill: string | { lang: string; level: string }
-      skillType: 'jobSkills' | 'softSkills' | 'languages'
+      skill: string | { lang: string; level: string };
+      skillType: 'jobSkills' | 'softSkills' | 'languages';
     }): void {
       if (e.skillType === 'languages') {
-        const newLang = e.skill as { lang: string; level: string }
+        const newLang = e.skill as { lang: string; level: string };
         const newLangIdx = this.formSettings.languages.findIndex(
           (lang) => lang.lang === newLang.lang
-        )
+        );
         if (newLangIdx < 0) {
           this.formSettings.languages = [
             ...new Set([...this.formSettings[e.skillType], e.skill]),
-          ] as { lang: string; level: string }[]
+          ] as { lang: string; level: string }[];
         }
       } else {
         this.formSettings[e.skillType] = [
           ...new Set([...this.formSettings[e.skillType], e.skill]),
-        ] as string[]
+        ] as string[];
       }
     },
     onRemoveSkill(e: {
-      skill: string
-      skillType: 'jobSkills' | 'softSkills'
+      skill: string;
+      skillType: 'jobSkills' | 'softSkills';
     }): void {
       this.formSettings[e.skillType] = [
         ...this.formSettings[e.skillType].filter((skill) => skill !== e.skill),
-      ]
+      ];
     },
     onUpdateSection(e: {
-      eventType: 'addEntry' | 'removeEntry'
-      sectionName: 'education' | 'work'
+      eventType: 'addEntry' | 'removeEntry';
+      sectionName: 'education' | 'work';
       entry: {
-        title: string
-        location: string
-        from: Date
-        to: Date
-        current: boolean
-        summary: string
-      }
+        title: string;
+        location: string;
+        from: Date;
+        to: Date;
+        current: boolean;
+        summary: string;
+      };
     }): void {
       if (e.eventType === 'addEntry') {
-        this.formSettings[e.sectionName].push(e.entry)
+        this.formSettings[e.sectionName].push(e.entry);
       } else {
         this.formSettings[e.sectionName] = this.formSettings[
           e.sectionName
-        ].filter((entry) => entry.title !== e.entry.title)
+        ].filter((entry) => entry.title !== e.entry.title);
       }
     },
     onUploadCv(e: any) {
-      this.formSettings = { ...e.formSettings }
+      this.formSettings = { ...e.formSettings };
     },
     onResetForm(): void {
       this.formSettings = {
@@ -188,15 +188,15 @@ export default Vue.extend({
         website: '',
         education: [],
         work: [],
-      }
-      localStorage.removeItem('cvSettings-es')
-      localStorage.removeItem('cvSettings-en')
+      };
+      localStorage.removeItem('cvSettings-es');
+      localStorage.removeItem('cvSettings-en');
     },
     isCvSettingsFromLocalStorageEmpty(item: any): boolean {
-      if (!item || Object.keys(item).length < 1) return true
+      if (!item || Object.keys(item).length < 1) return true;
       return Object.keys(item).every(
         (key) => item[key] === '' || item[key].length < 1
-      )
+      );
     },
   },
   head(): NuxtOptionsHead {
@@ -225,6 +225,11 @@ export default Vue.extend({
             'cv, curriculum, resume, curriculum vitae, cv maker, cv generator, nuxt, vue, tailwindcss',
         },
         {
+          hid: 'twitter:creator',
+          name: 'twitter:creator',
+          content: '@claudiabdm',
+        },
+        {
           hid: 'twitter:title',
           name: 'twitter:title',
           content: this.$t('title-tag') as string,
@@ -234,10 +239,21 @@ export default Vue.extend({
           name: 'twitter:description',
           content: this.$t('description') as string,
         },
+        {
+          hid: 'twitter:card',
+          name: 'twitter:card',
+          content: 'summary_large_image',
+        },
+        {
+          hid: 'twitter:image',
+          name: 'twitter:image',
+          content:
+            'https://repository-images.githubusercontent.com/315020727/e23c6200-8110-11eb-87b8-e9e5757bd827',
+        },
       ],
-    }
+    };
   },
-})
+});
 </script>
 <style lang="postcss" scoped>
 @media screen and (min-width: 1024px) {
