1b5c8707be7ad7eaa5b1e6c36810bada5de8b52d
Mikuu <biaofu@thoughtworks.com>
2024-02-20 21:41:15 +0800

    Add support for Chinese, also fix an issue that not apply local language to 'Social' in settings (#34)

--- components/CvSettings.vue
+++ components/CvSettings.vue
@@ -122,9 +122,9 @@
 
       <!-- SOCIAL -->
       <fieldset class="form__section grid gap-3">
-        <expansion-panel :panel-name="'Social'">
+        <expansion-panel :panel-name="$t('social')">
           <template v-slot:title>
-            <legend class="form__legend">Social</legend>
+            <legend class="form__legend">{{ $t('social') }}</legend>
           </template>
           <template v-slot:content>
             <div class="grid grid-cols-2 gap-x-3 gap-y-10">
@@ -261,6 +261,9 @@ const config = {
     { name: 'en-name', code: 'en' },
     { name: 'id-name', code: 'id' },
     { name: 'fr-name', code: 'fr' },
+    { name: 'zh-name', code: 'zh' },
+    { name: 'de-name', code: 'de' },
+    { name: 'ar-name', code: 'ar' },
   ],
 };
 
