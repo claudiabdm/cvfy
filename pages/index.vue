372960ebb10d86650c83a2e15f6208d6dcb76bdd
claudiabdm <claudiabdm@hotmail.es>
2024-02-23 17:13:56 +0100

    Use head hook

--- pages/index.vue
+++ pages/index.vue
@@ -1,102 +1,94 @@
 <template>
   <div class="main">
-    <cv-settings></cv-settings>
-    <cv-preview></cv-preview>
+    <CvSettings />
+    <CvPreview />
   </div>
 </template>
 
-<script lang="ts">
-import { defineComponent } from 'vue';
-import CvSettings from '@/components/CvSettings.vue';
-import CvPreview from '@/components/CvPreview.vue';
-
+<script setup lang="ts">
 const CVFY_IMAGE = 'https://cvfy.xyz/CvFy-no-border.png';
 
-export default defineComponent({
-  name: 'Index',
-  components: { CvSettings, CvPreview },
-  head() {
-    return {
-      htmlAttrs: {
-        lang: this.$i18n.locale,
-      },
-      title: this.$t('title-tag') as string,
-      link: [
-        {
-          rel: 'preload',
-          as: 'image',
-          href: 'https://cdn.ko-fi.com/cdn/kofi5.png?v=3',
-        },
-        {
-          rel: 'canonical',
-          href: 'https://cvfy.xyz/',
-        },
-      ],
-      meta: [
-        {
-          hid: 'description',
-          name: 'description',
-          content: this.$t('description') as string,
-        },
-        {
-          hid: 'author',
-          name: 'author',
-          content: 'claudiabdm',
-        },
-        {
-          hid: 'og:image',
-          property: 'og:image',
-          content: CVFY_IMAGE,
-        },
-        {
-          hid: 'og:author',
-          property: 'og:author',
-          content: 'Claudia Benito',
-        },
-        {
-          hid: 'og:title',
-          property: 'og:title',
-          content: this.$t('title-tag') as string,
-        },
-        {
-          hid: 'og:description',
-          property: 'og:description',
-          content: this.$t('description') as string,
-        },
-        {
-          hid: 'twitter:creator',
-          name: 'twitter:creator',
-          content: '@claudiabdm',
-        },
-        {
-          hid: 'twitter:title',
-          name: 'twitter:title',
-          content: this.$t('title-tag') as string,
-        },
-        {
-          hid: 'twitter:description',
-          name: 'twitter:description',
-          content: this.$t('description') as string,
-        },
-        {
-          hid: 'twitter:card',
-          name: 'twitter:card',
-          content: 'summary_large_image',
-        },
-        {
-          hid: 'twitter:image',
-          name: 'twitter:image',
-          content: CVFY_IMAGE,
-        },
-        {
-          hid: 'twitter:image:alt',
-          name: 'twitter:image:alt',
-          content: this.$t('description') as string,
-        },
-      ],
-    };
+const { t, locale } = useI18n();
+useHead({
+  htmlAttrs: {
+    lang: locale,
   },
+  title: t('title-tag'),
+  link: [
+    {
+      rel: 'preload',
+      as: 'image',
+      href: 'https://cdn.ko-fi.com/cdn/kofi5.png?v=3',
+    },
+    {
+      rel: 'canonical',
+      href: 'https://cvfy.xyz/',
+    },
+  ],
+  meta: [
+    {
+      hid: 'description',
+      name: 'description',
+      content: t('description'),
+    },
+    {
+      hid: 'author',
+      name: 'author',
+      content: 'claudiabdm',
+    },
+    {
+      hid: 'og:image',
+      property: 'og:image',
+      content: CVFY_IMAGE,
+    },
+    {
+      hid: 'og:author',
+      property: 'og:author',
+      content: 'Claudia Benito',
+    },
+    {
+      hid: 'og:title',
+      property: 'og:title',
+      content: t('title-tag'),
+    },
+    {
+      hid: 'og:description',
+      property: 'og:description',
+      content: t('description'),
+    },
+    {
+      hid: 'twitter:creator',
+      name: 'twitter:creator',
+      content: '@claudiabdm',
+    },
+    {
+      hid: 'twitter:title',
+      name: 'twitter:title',
+      content: t('title-tag'),
+    },
+    {
+      hid: 'twitter:description',
+      name: 'twitter:description',
+      content: t('description'),
+    },
+    {
+      hid: 'twitter:card',
+      name: 'twitter:card',
+      content: 'summary_large_image',
+    },
+    {
+      hid: 'twitter:image',
+      name: 'twitter:image',
+      content: CVFY_IMAGE,
+    },
+    {
+      hid: 'twitter:image:alt',
+      name: 'twitter:image:alt',
+      content: t('description'),
+    },
+  ],
 });
+
 </script>
 <style lang="postcss" scoped>
 @media screen and (min-width: 1024px) {
