65299e0c3623bef6fe061b9da2357c01c6a5fc93
claudiabdm <44007726+claudiabdm@users.noreply.github.com>
2024-06-05 22:03:24 +0200

    Add landing page

---
+++ components/landing/LandingLogo.vue
@@ -0,0 +1,15 @@
+<template>
+  <h1 class="[grid-area:logo]">
+    <NuxtLink
+      to="/"
+      class="bg-none font-logo uppercase tracking-widest"
+      active-class="bg-none"
+    >
+      <span
+        class="font-bold text-fl-lg text-transparent bg-gradient-to-br from-violet-800 to-fuchsia-700 bg-clip-text hover:opacity-80"
+      >
+        CvFy
+      </span>
+    </NuxtLink>
+  </h1>
+</template>
--- pages/index.vue
+++ pages/index.vue
@@ -1,109 +1,58 @@
-<script setup lang="ts">
-const CVFY_IMAGE = 'https://cvfy.xyz/CvFy-no-border.png'
-
-const route = useRoute()
-const { t, locale } = useI18n()
-
-const href = `https://cvfy.xyz${route.path}`
-
-useHead({
-  htmlAttrs: {
-    lang: locale,
-  },
-  title: t('title-tag'),
-  link: [
-    {
-      rel: 'preload',
-      as: 'image',
-      href: 'https://cdn.ko-fi.com/cdn/kofi5.png?v=3',
-    },
-    {
-      rel: 'canonical',
-      href,
-    },
-  ],
-  meta: [
-    {
-      hid: 'description',
-      name: 'description',
-      content: t('description'),
-    },
-    {
-      hid: 'author',
-      name: 'author',
-      content: 'claudiabdm',
-    },
-    {
-      hid: 'og:image',
-      property: 'og:image',
-      content: CVFY_IMAGE,
-    },
-    {
-      hid: 'og:author',
-      property: 'og:author',
-      content: 'Claudia Benito',
-    },
-    {
-      hid: 'og:title',
-      property: 'og:title',
-      content: t('title-tag'),
-    },
-    {
-      hid: 'og:description',
-      property: 'og:description',
-      content: t('description'),
-    },
-    {
-      hid: 'twitter:creator',
-      name: 'twitter:creator',
-      content: '@claudiabdm',
-    },
-    {
-      hid: 'twitter:title',
-      name: 'twitter:title',
-      content: t('title-tag'),
-    },
-    {
-      hid: 'twitter:url',
-      name: 'twitter:url',
-      content: href,
-    },
-    {
-      hid: 'twitter:description',
-      name: 'twitter:description',
-      content: t('description'),
-    },
-    {
-      hid: 'twitter:card',
-      name: 'twitter:card',
-      content: 'summary_large_image',
-    },
-    {
-      hid: 'twitter:image',
-      name: 'twitter:image',
-      content: CVFY_IMAGE,
-    },
-    {
-      hid: 'twitter:image:alt',
-      name: 'twitter:image:alt',
-      content: t('description'),
-    },
-  ],
+<script lang="ts" setup>
+definePageMeta({
+  titleI18n: 'title-tag',
+  layout: 'landing',
 })
 </script>
 
 <template>
-  <main class="font-sans main">
-    <CvSettings class="basis-1/4 min-w-80" />
-    <CvPreview class="basis-3/4" />
-  </main>
+  <section
+    class="min-h-[calc(100vh-var(--header-height))] flex flex-col justify-center items-center text-center mx-auto gap-8 flex-wrap font-bold p-fl-md"
+  >
+    <div class="leading-normal">
+      <h2 class="mb-2 text-fl-2xl">
+        {{ $t('hero-title-1') }} <span
+          class="text-nowrap text-transparent bg-gradient-to-bl from-violet-800 to-fuchsia-700 bg-clip-text"
+        >{{ $t('hero-title-2') }}</span><br> {{ $t('hero-title-3') }}<span
+          class="text-fl-2xl text-transparent bg-gradient-to-br from-violet-800 to-fuchsia-700 bg-clip-text"
+        >.</span>
+      </h2>
+      <p class="mb-4 font-light max-w-prose mx-auto">
+        {{ $t('hero-paragraph') }}
+      </p>
+      <LandingLink
+        to="create"
+        cta
+      >
+        <span>{{ $t('get-started') }}</span>
+        <svg
+          xmlns="http://www.w3.org/2000/svg"
+          fill="none"
+          viewBox="0 0 24 24"
+          stroke-width="2"
+          stroke="currentColor"
+          class="size-4"
+        >
+          <path
+            stroke-linecap="round"
+            stroke-linejoin="round"
+            d="M13.5 4.5 21 12m0 0-7.5 7.5M21 12H3"
+          />
+        </svg>
+      </LandingLink>
+    </div>
+    <LandingHeroImage />
+    <div class="text-xs font-light">
+      Made with ♥️ by
+      <LandingLink
+        class="underline"
+        to="https://github.com/claudiabdm"
+        external
+      >
+        <span class="text-violet-800">
+          claudiabdm
+        </span>
+      </LandingLink>
+    </div>
+  </section>
 </template>
-
-<style lang="postcss">
-@import '@/assets/styles/form.postcss';
-@media screen and (min-width: 1024px) {
-  .main {
-    @apply flex h-screen overflow-hidden;
-  }
-}
-</style>
