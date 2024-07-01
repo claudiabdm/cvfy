faa112ed72ae3fa58e99b647f9e05226f61030b1
claudiabdm <44007726+claudiabdm@users.noreply.github.com>
2024-07-01 14:45:39 +0200

    Install pdfkit

---
+++ types/index.d.ts
@@ -0,0 +1,11 @@
+declare module 'pdfkit/js/pdfkit.standalone' {
+  import PDF from 'pdfkit'
+
+  export = PDF
+}
+
+declare module 'blob-stream/.js' {
+  import blobStream from 'blob-stream'
+
+  export = blobStream
+}
