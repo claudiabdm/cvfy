4076acbb43cac979f9e0e189652f701a7f035a73
claudiabdm <44007726+claudiabdm@users.noreply.github.com>
2026-01-13 16:57:13 +0100

    Fix double-line skills in one-column layout and reduce cv section gap

--- components/CvPreviewSkill.vue
+++ components/CvPreviewSkill.vue
@@ -1,11 +1,13 @@
-<script lang="ts" setup>
+<script lang="ts" setup generic="T extends boolean">
 import { useCvState } from '~/data/useCvState'
+import type { LanguagesSkill } from '~/types/cvfy'
 
 defineProps<{
   display?: boolean
-  skills: string[]
+  skills: T extends true ? LanguagesSkill['skill'][] : string[]
   skillName: string
   withTags?: boolean
+  isLanguage?: T
 }>()
 
 const { formSettings } = useCvState()
@@ -14,26 +16,70 @@ const { formSettings } = useCvState()
 <template>
   <section
     v-if="display"
-    class="cv__section"
-    :class="formSettings.layout === 'one-column' && 'flex gap-1'"
+    class="cv__section preview-skill"
+    :class="formSettings.layout === 'one-column' && 'inline-block w-full'"
   >
     <h3
       class="capitalize"
-      :class="formSettings.layout === 'one-column' ? 'two-dots inline flex-shrink-0' : 'cv__section-title'"
+      :class="formSettings.layout === 'one-column' ? 'two-dots inline' : 'cv__section-title'"
     >
       {{ skillName }}
     </h3>
     <ul
       class="font-light"
-      :class="[{ 'flex flex-wrap': formSettings.layout === 'one-column' }, { cv__list: formSettings.layout !== 'one-column' && !withTags }, { cv__tags: withTags }]"
+      :class="[{ inline: formSettings.layout === 'one-column' }, { cv__list: formSettings.layout !== 'one-column' && !withTags && !isLanguage }, { cv__tags: withTags }]"
     >
       <li
         v-for="skill in skills"
         :key="`preview${skill}`"
-        :class="[{ comma: formSettings.layout === 'one-column' }, { cv__tag: withTags }]"
+        :class="[{ 'flex justify-between': isLanguage && formSettings.layout === 'two-column' }, { inline: formSettings.layout === 'one-column' }, { comma: formSettings.layout === 'one-column' }, { cv__tag: withTags }]"
       >
-        {{ skill }}
+        <template v-if="isLanguage">
+          <span
+            :class="formSettings.layout === 'one-column' ? 'font-light' : 'font-medium'"
+          >
+            {{ (skill as LanguagesSkill['skill']).lang }}
+          </span>
+          <span class="font-light ml-auto">
+            <template v-if="formSettings.layout === 'one-column'">
+              (</template>{{
+              $t((skill as LanguagesSkill['skill']).level)
+            }}<template v-if="formSettings.layout === 'one-column'">)</template>
+          </span>
+        </template>
+
+        <template v-else>
+          {{ skill }}
+        </template>
       </li>
     </ul>
   </section>
 </template>
+
+<style lang="postcss" scoped>
+.comma {
+  white-space: preserve;
+}
+
+ul :not(li:last-child).comma::after {
+  content: ', ';
+}
+
+ul li:last-child.comma::after {
+  content: '.';
+}
+
+.two-dots {
+  &::after {
+    content: ': ';
+  }
+}
+
+.preview-skill {
+  margin-bottom: 0.25rem;
+
+  &:last-child {
+    margin-bottom: 0;
+  }
+}
+</style>
