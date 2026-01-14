83ae8e8a3b3171f1f7b1553ffe55192cfc56c11f
claudiabdm <44007726+claudiabdm@users.noreply.github.com>
2026-01-14 11:39:21 +0100

    Fix skill text when copy/pasting in one-col layout

--- components/CvPreviewSkill.vue
+++ components/CvPreviewSkill.vue
@@ -11,6 +11,26 @@ defineProps<{
 }>()
 
 const { formSettings } = useCvState()
+
+function formatSkill(skillText: string, index: number, length: number) {
+  if (formSettings.value.layout !== 'one-column') {
+    return skillText
+  }
+
+  return `${skillText}${withPunctuation(index, length)}`
+}
+
+function formatLangLevel(langLevel: LanguagesSkill['skill']['lang'], index: number, length: number) {
+  if (formSettings.value.layout !== 'one-column') {
+    return langLevel
+  }
+
+  return ` (${langLevel})${withPunctuation(index, length)}`
+}
+
+function withPunctuation(index: number, length: number) {
+  return formSettings.value.layout === 'one-column' && index < length - 1 ? ', ' : '.'
+}
 </script>
 
 <template>
@@ -30,9 +50,9 @@ const { formSettings } = useCvState()
       :class="[{ inline: formSettings.layout === 'one-column' }, { cv__list: formSettings.layout !== 'one-column' && !withTags && !isLanguage }, { cv__tags: withTags }]"
     >
       <li
-        v-for="skill in skills"
+        v-for="(skill, i) in skills"
         :key="`preview${skill}`"
-        :class="[{ 'flex justify-between': isLanguage && formSettings.layout === 'two-column' }, { inline: formSettings.layout === 'one-column' }, { comma: formSettings.layout === 'one-column' }, { cv__tag: withTags }]"
+        :class="[{ 'flex justify-between': isLanguage && formSettings.layout === 'two-column' }, { inline: formSettings.layout === 'one-column' }, { cv__tag: withTags }]"
       >
         <template v-if="isLanguage">
           <span
@@ -40,16 +60,14 @@ const { formSettings } = useCvState()
           >
             {{ (skill as LanguagesSkill['skill']).lang }}
           </span>
-          <span class="font-light ml-auto">
-            <template v-if="formSettings.layout === 'one-column'">
-              (</template>{{
-              $t((skill as LanguagesSkill['skill']).level)
-            }}<template v-if="formSettings.layout === 'one-column'">)</template>
+          <span class="font-light ml-auto whitespace-nowrap">
+            {{
+              formatLangLevel($t((skill as LanguagesSkill['skill']).level), i, skills.length)
+            }}
           </span>
         </template>
-
         <template v-else>
-          {{ skill }}
+          {{ formatSkill(skill as string, i, skills.length) }}
         </template>
       </li>
     </ul>
